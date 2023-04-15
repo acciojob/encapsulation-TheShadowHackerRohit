0e83505fd6e02a0fb53515736844f806c649a32c
rohit <rohitme0091@gmail.com>
2023-04-15 11:30:34 +0530

    done all task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        //System.out.println(obj.name);
+
+        obj.setName("Rohit");
+
+        String ans = obj.getName();
+
+        System.out.println(ans);
+    }
 }
